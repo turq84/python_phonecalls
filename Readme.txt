149be455409db270da37fd022d69d13712146dc8
turq84 <kayla.m.gordon@gmail.com>
2017-07-05 16:53:27 +0300

    This Python program allows you to make conference calls.
    For more information, please see the Readme.txt file.

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+This python file requires an account with Twilio (www.twilio.com). From there you can acquire a phone number and use their API.
+
+For more information and instruction, please see https://www.fullstackpython.com/blog/make-phone-calls-python.html.
---
+++ python_phonecalls.py
@@ -0,0 +1,32 @@
+from twilio.rest import TwilioRestClient
+
+
+# Twilio phone number goes here. Grab one at https://twilio.com/try-twilio
+# and use the E.164 format, for example: "+12025551234"
+TWILIO_PHONE_NUMBER = ""
+
+# list of one or more phone numbers to dial, in "+19732644210" format
+# for multiple numbers, separate each by a comma. Ie: DIAL_NUMBERS = ["+555444333333", "+555444333333", "+555444333333"].
+DIAL_NUMBERS = [""]
+
+# URL location of TwiML instructions for how to handle the phone call
+TWIML_INSTRUCTIONS_URL = \
+  "http://static.fullstackpython.com/phone-calls-python.xml"
+
+# replace the placeholder values with your Account SID and Auth Token
+# found on the Twilio Console: https://www.twilio.com/console
+client = TwilioRestClient("Account SID#", "Authentication Token#")
+
+
+def dial_numbers(numbers_list):
+    """Dials one or more phone numbers from a Twilio phone number."""
+    for number in numbers_list:
+        print("Dialing " + number)
+        # set the method to "GET" from default POST because Amazon S3 only
+        # serves GET requests on files. Typically POST would be used for apps
+        client.calls.create(to=number, from_=TWILIO_PHONE_NUMBER,
+                            url=TWIML_INSTRUCTIONS_URL, method="GET")
+
+
+if __name__ == "__main__":
+    dial_numbers(DIAL_NUMBERS)
